420cfa4759ae24d1d55a17d60a8a1c9126e913f1
LihongGroup <lihonggroup@outlook.com>
2026-04-13 20:51:09 +0800

    Auto send: 2026-04-13 20:51:08

--- lingbot-va/script/run_extract_latents.sh
+++ lingbot-va/script/run_extract_latents.sh
@@ -3,9 +3,6 @@
 # Configure GPU devices if needed (e.g. 0,1,2,3)
 export CUDA_VISIBLE_DEVICES=0
 
-# Disable huggingface network requests to use local checkpoints
-export HF_HUB_OFFLINE=1
-
 python wan_va/extract_latents.py \
     --dataset_path ./Data/handcap_lingbot \
     --ckpt_path ./ckpt/lingbot-va-base \
