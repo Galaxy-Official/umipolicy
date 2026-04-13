72fb480e72deaac7cfdc798b9b9700aa0f79255a
LihongGroup <lihonggroup@outlook.com>
2026-04-13 14:54:56 +0800

    Auto send: 2026-04-13 14:54:56

--- lingbot-va/script/run_va_posttrain_handcap.sh
+++ lingbot-va/script/run_va_posttrain_handcap.sh
@@ -16,10 +16,11 @@ if [ $# -ne 0 ]; then
     overrides="$*"
 fi
 
-export WANDB_API_KEY="your key"
-export WANDB_BASE_URL="your url"
-export WANDB_TEAM_NAME="your team name"
-export WANDB_PROJECT="your project"
+# export WANDB_API_KEY="your key"
+# export WANDB_BASE_URL="your url"
+# export WANDB_TEAM_NAME="your team name"
+# export WANDB_PROJECT="your project"
+export WANDB_MODE="disabled"
 
 ## node setting
 num_gpu=${NGPU}
