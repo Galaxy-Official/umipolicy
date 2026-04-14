f5eb8028a80599c75daba6e55738f5cd1b37af85
LihongGroup <lihonggroup@outlook.com>
2026-04-14 16:25:45 +0800

    fix dataset configuration for handcap action dimension

--- lingbot-va/wan_va/configs/va_handcap_cfg.py
+++ lingbot-va/wan_va/configs/va_handcap_cfg.py
@@ -16,7 +16,7 @@ va_handcap_cfg.env_type = 'handcap'
 
 va_handcap_cfg.height = 256
 va_handcap_cfg.width = 320
-va_handcap_cfg.action_dim = 30
+va_handcap_cfg.action_dim = 10
 va_handcap_cfg.action_per_frame = 16
 va_handcap_cfg.obs_cam_keys = [
     'observation.images.wrist', 'observation.tactiles.left',
@@ -32,8 +32,7 @@ va_handcap_cfg.action_num_inference_steps = 50
 va_handcap_cfg.snr_shift = 5.0
 va_handcap_cfg.action_snr_shift = 1.0
 
-va_handcap_cfg.used_action_channel_ids = list(range(0, 7)) + list(
-    range(28, 29)) + list(range(7, 14)) + list(range(29, 30))
+va_handcap_cfg.used_action_channel_ids = list(range(0, 10))
 inverse_used_action_channel_ids = [
     len(va_handcap_cfg.used_action_channel_ids)
 ] * va_handcap_cfg.action_dim
@@ -43,14 +42,6 @@ va_handcap_cfg.inverse_used_action_channel_ids = inverse_used_action_channel_ids
 
 va_handcap_cfg.action_norm_method = 'quantiles'
 va_handcap_cfg.norm_stat = {
-    "q01": [
-        -0.06172713458538055, -3.6716461181640625e-05, -0.08783501386642456,
-        -1, -1, -1, -1, -0.3547105032205582, -1.3113021850585938e-06,
-        -0.11975435614585876, -1, -1, -1, -1
-    ] + [0.] * 16,
-    "q99": [
-        0.3462600058317184, 0.39966784834861746, 0.14745532035827624, 1, 1, 1,
-        1, 0.034201726913452024, 0.39142737388610793, 0.1792279863357542, 1, 1,
-        1, 1
-    ] + [0.] * 14 + [1.0, 1.0],
+    "q01": [-1.0] * 10,
+    "q99": [1.0] * 10,
 }
