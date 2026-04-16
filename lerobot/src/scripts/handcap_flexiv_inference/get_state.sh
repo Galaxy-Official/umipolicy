18125af42673d78d8974f15b8007a95163c42861
LihongGroup <lihonggroup@outlook.com>
2026-04-16 20:41:03 +0800

    Auto send: 2026-04-16 20:41:03

---
+++ lerobot/src/scripts/handcap_flexiv_inference/get_state.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 获取目前所在的工作空间目录，并设置 PYTHONPATH
+export PYTHONPATH="$(pwd)/lerobot/src:$(pwd):$PYTHONPATH"
+
+# (可选) 设置 CUDA 环境
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-$CUDA_VERSION/lib64"
+# export PATH="/usr/local/cuda-$CUDA_VERSION/bin:$PATH"
+# export CUDA_HOME="/usr/local/cuda-$CUDA_VERSION"
+
+python -m lerobot.get_real_state.read_current_pos
