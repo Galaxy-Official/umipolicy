3d76999d87c7c6bef77d9f7804783a06f1a4c971
LihongGroup <lihonggroup@outlook.com>
2026-04-18 23:42:52 +0800

    Auto send: 2026-04-18 23:42:52

--- lerobot/src/lerobot/scripts/lerobot_flexiv_teleop_record.py
+++ lerobot/src/lerobot/scripts/lerobot_flexiv_teleop_record.py
@@ -46,6 +46,34 @@ class LeaderFKResolver:
         "so100": "urdf/assets/SO_5DOF_ARM100_8j_URDF.SLDASM/urdf/SO_5DOF_ARM100_8j_URDF.SLDASM.urdf",
     }
 
+    def matrix2quaternion(self, matrix):
+        tr = matrix[0, 0] + matrix[1, 1] + matrix[2, 2]
+        if tr > 0:
+            S = np.sqrt(tr + 1.0) * 2
+            qw = 0.25 * S
+            qx = (matrix[2, 1] - matrix[1, 2]) / S
+            qy = (matrix[0, 2] - matrix[2, 0]) / S
+            qz = (matrix[1, 0] - matrix[0, 1]) / S
+        elif matrix[0, 0] > matrix[1, 1] and matrix[0, 0] > matrix[2, 2]:
+            S = np.sqrt(1.0 + matrix[0, 0] - matrix[1, 1] - matrix[2, 2]) * 2
+            qw = (matrix[2, 1] - matrix[1, 2]) / S
+            qx = 0.25 * S
+            qy = (matrix[0, 1] + matrix[1, 0]) / S
+            qz = (matrix[0, 2] + matrix[2, 0]) / S
+        elif matrix[1, 1] > matrix[2, 2]:
+            S = np.sqrt(1.0 + matrix[1, 1] - matrix[0, 0] - matrix[2, 2]) * 2
+            qw = (matrix[0, 2] - matrix[2, 0]) / S
+            qx = (matrix[0, 1] + matrix[1, 0]) / S
+            qy = 0.25 * S
+            qz = (matrix[1, 2] + matrix[2, 1]) / S
+        else:
+            S = np.sqrt(1.0 + matrix[2, 2] - matrix[0, 0] - matrix[1, 1]) * 2
+            qw = (matrix[1, 0] - matrix[0, 1]) / S
+            qx = (matrix[0, 2] + matrix[2, 0]) / S
+            qy = (matrix[1, 2] + matrix[2, 1]) / S
+            qz = 0.25 * S
+        return np.array([qx, qy, qz, qw])
+
     def __init__(self, robot_type="koch"):
         self.robot_type = robot_type
         # Pybullet GUI overhead avoidance
@@ -73,10 +101,10 @@ class LeaderFKResolver:
             data = [
                 -dynamixel_joints[0],
                 90 - dynamixel_joints[1],
-                90 - dynamixel_joints[2],
-                90 - dynamixel_joints[3],
-                dynamixel_joints[4] - 90,
-                -dynamixel_joints[5],
+                dynamixel_joints[2] - 90,
+                dynamixel_joints[3] - 90,
+                90 - dynamixel_joints[4],
+                dynamixel_joints[5],
             ]
             data = [angle * np.pi / 180 for angle in data]
             for i, joint in enumerate(data):
@@ -85,7 +113,29 @@ class LeaderFKResolver:
             # eef Link #4
             eef_pos = np.array(pb.getLinkState(self.robot_pb, 4)[0])
             eef_orn = pb.getLinkState(self.robot_pb, 4)[1]
-            return eef_pos, eef_orn, -data[-1]
+            
+            eef_orn_matrix = np.array(pb.getMatrixFromQuaternion(eef_orn)).reshape(3, 3)
+            original_axes = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+            z_in_eef = np.array([-1, 0, 0])
+            x_in_eef = np.array([0, -1, 0])
+            y_in_eef = np.array([0, 0, 1])
+            new_axes = np.array([x_in_eef, y_in_eef, z_in_eef]).T
+            new_axes = np.concatenate([new_axes, np.array([[1, 1, 1]])], axis=0)
+
+            x_ab = np.array([np.dot(eef_orn_matrix[:, 0], original_axes[0]), np.dot(eef_orn_matrix[:, 0], original_axes[1]), np.dot(eef_orn_matrix[:, 0], original_axes[2])])
+            y_ab = np.array([np.dot(eef_orn_matrix[:, 1], original_axes[0]), np.dot(eef_orn_matrix[:, 1], original_axes[1]), np.dot(eef_orn_matrix[:, 1], original_axes[2])])
+            z_ab = np.array([np.dot(eef_orn_matrix[:, 2], original_axes[0]), np.dot(eef_orn_matrix[:, 2], original_axes[1]), np.dot(eef_orn_matrix[:, 2], original_axes[2])])
+
+            T_ab = np.array([x_ab, y_ab, z_ab, eef_pos]).T
+            T_ab = np.concatenate([T_ab, np.array([[0, 0, 0, 1]])], axis=0)
+
+            rot_axes = T_ab @ new_axes
+            rot_axes = rot_axes[:3]
+            
+            new_eef_orn = self.matrix2quaternion(rot_axes)
+            
+            return eef_pos, new_eef_orn, -data[-1]
 
         elif self.robot_type == "so100":
             data = [
