1841c9fb4f0ea92c8baf4d7004f6b6b53ebe4ce7
LihongGroup <lihonggroup@outlook.com>
2026-04-19 13:53:33 +0800

    Auto send: 2026-04-19 13:53:33

--- lerobot/src/lerobot/scripts/lerobot_flexiv_teleop_record.py
+++ lerobot/src/lerobot/scripts/lerobot_flexiv_teleop_record.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--robot-path",
         type=str,
-        default="lerobot/configs/robot/koch.yaml",
+        default="configs/robot/koch.yaml",
         help="Path to robot yaml file used to instantiate the robot using `make_robot` factory function.",
     )
     parser.add_argument(
