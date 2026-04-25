73902fd2f433ccfdb4da339e4ff1244ee409eb26
LihongGroup <lihonggroup@outlook.com>
2026-04-25 22:07:42 +0800

    Auto send: 2026-04-25 22:07:42

--- lerobot/src/scripts/replay/lerobot_replay.sh
+++ lerobot/src/scripts/replay/lerobot_replay.sh
@@ -21,5 +21,5 @@ echo "====================================================="
 
 python "${SRC_DIR}/lerobot/scripts/lerobot_replay_train.py" \
     --data_root "Data/replay/simple_sorting_409/handcap" \
-    --episode_index 5 \
+    --episode_index 40 \
     --task_name "eval_lerobot_replay"
