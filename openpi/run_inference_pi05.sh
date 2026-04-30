2def6815587422478a31834a313fadfaaeac88d9
LihongGroup <lihonggroup@outlook.com>
2026-04-30 15:34:54 +0800

    Auto send: 2026-04-30 15:34:54

--- openpi/run_inference_pi05.sh
+++ openpi/run_inference_pi05.sh
@@ -6,12 +6,13 @@ export FLEXIV_ROBOT_IP="192.168.2.100"
 export FLEXIV_LOCAL_IP="192.168.2.102"
 export FLEXIV_ROBOT_SN="${FLEXIV_ROBOT_SN:-Rizon4-062339}"
 export FLEXIV_GRIPPER_NAME="${FLEXIV_GRIPPER_NAME:-Flexiv-GN01}"
+export FLEXIV_INIT_POSE="${FLEXIV_INIT_POSE:-[-0.0009,-0.2370,-0.0133,2.1459,-0.0058,0.8170,-0.0012]}"
 
 POLICY_CONFIG="${POLICY_CONFIG:-pi05_simple_sorting}"
-POLICY_DIR="${POLICY_DIR:-~/umipolicy/openpi/ckpt/99999}"
+POLICY_DIR="${POLICY_DIR:-/home/rhos/umipolicy/openpi/ckpt/70000}"
 PROMPT="${PROMPT:-simple sorting task}"
 
-CTRL_FREQ="${CTRL_FREQ:-20}"
+CTRL_FREQ="${CTRL_FREQ:-1}"
 STEPS_PER_INFERENCE="${STEPS_PER_INFERENCE:-4}"
 OBS_HORIZON="${OBS_HORIZON:-2}"
 ACTION_LATENCY="${ACTION_LATENCY:-0.0}"
@@ -26,5 +27,6 @@ bash start_handcap_remote_inference.sh \
   --steps-per-inference "${STEPS_PER_INFERENCE}" \
   --obs-horizon "${OBS_HORIZON}" \
   --action-latency "${ACTION_LATENCY}" \
+  --init-qpos "${FLEXIV_INIT_POSE}" \
   --startup-timeout 1800 \
   --no-use-tactile
