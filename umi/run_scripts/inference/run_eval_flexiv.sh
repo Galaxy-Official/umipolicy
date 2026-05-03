0e52e64add4ea5ae54b0b27496cdc6a1130a7642
LihongGroup <lihonggroup@outlook.com>
2026-05-03 14:59:33 +0800

    Auto send: 2026-05-03 14:59:33

--- umi/run_scripts/inference/run_eval_flexiv.sh
+++ umi/run_scripts/inference/run_eval_flexiv.sh
@@ -18,11 +18,11 @@ export FLEXIV_GRIPPER_NAME="Flexiv-GN01"
 export FLEXIV_INIT_POSE="[0.1691,-0.2370,-0.0133,2.1459,-0.0058,0.8170,0.2154]"
 
 # 预训练模型 Checkpoint 路径
-CKPT_PATH="ckpt/501breadmoving_490.ckpt"
+CKPT_PATH="ckpt/430towelhanging_train_490.ckpt"
 
 # 设置数据保存输出路径
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-OUTPUT_DIR="data_local/501breadmoving/eval_recordings/eval_${TIMESTAMP}"
+OUTPUT_DIR="data_local/430towelhanging/eval_recordings/eval_${TIMESTAMP}"
 
 echo "=========================================="
 echo "🚀 准备启动 UMI 真机推理 (Policy Inference)"
