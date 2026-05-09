6a3dd4ca404a3f7aa7e7fc14dd405345441fdb27
LihongGroup <lihonggroup@outlook.com>
2026-05-09 20:08:39 +0800

    Auto send: 2026-05-09 20:08:39

--- openpi/scripts/handcap_training/506_open_bottle/train_health_distill_tactile_wrist_force.sh
+++ openpi/scripts/handcap_training/506_open_bottle/train_health_distill_tactile_wrist_force.sh
@@ -32,7 +32,7 @@ HEALTH_REPO_IDS=(
 HEALTH_LABELS=("0" "50" "100")
 
 BATCH_SIZE="${BATCH_SIZE:-144}"
-NUM_TRAIN_STEPS="${NUM_TRAIN_STEPS:-50000}"
+NUM_TRAIN_STEPS="${NUM_TRAIN_STEPS:-100000}"
 SAVE_INTERVAL="${SAVE_INTERVAL:-5000}"
 # This workload is video-decode bound; the latest profile starves the GPUs with
 # 16 workers, while 32 workers keeps Data avg much closer to model time.
