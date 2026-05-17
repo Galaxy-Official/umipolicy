5a2b6fefaec5bcb59a2a1a89e29af1e8b35f3081
LihongGroup <lihonggroup@outlook.com>
2026-05-17 13:12:44 +0800

    Auto send: 2026-05-17 13:12:44

--- openpi/run_inference_pi05.sh
+++ openpi/run_inference_pi05.sh
@@ -37,13 +37,13 @@ export RECORDING_INDEX="${RECORDING_INDEX:-}"
 
 # close bottle
 # 3. 任务策略名称（需与 handcap_config.py 中的 registered name 对应）
-POLICY_CONFIG="${POLICY_CONFIG:-pi05_512_close_bottle_350}"
+# POLICY_CONFIG="${POLICY_CONFIG:-pi05_512_close_bottle_350}"
 
-# 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
-POLICY_DIR="${POLICY_DIR:-ckpt/lihong/512_close_bottle_350_vision_30000}"
+# # 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
+# POLICY_DIR="${POLICY_DIR:-ckpt/lihong/512_close_bottle_350_vision_30000}"
 
-# 5. 任务 Prompt 提示词（输入给模型的语言指令）
-PROMPT="${PROMPT:-Pick up the bottle cap and cover it with the lid.}"
+# # 5. 任务 Prompt 提示词（输入给模型的语言指令）
+# PROMPT="${PROMPT:-Pick up the bottle cap and cover it with the lid.}"
 
 # stiring
 # 3. 任务策略名称（需与 handcap_config.py 中的 registered name 对应）
@@ -68,13 +68,13 @@ PROMPT="${PROMPT:-Pick up the bottle cap and cover it with the lid.}"
 
 #  erase board
 # 3. 任务策略名称（需与 handcap_config.py 中的 registered name 对应）
-# POLICY_CONFIG="${POLICY_CONFIG:-pi05_510_erase_board_350}"
+POLICY_CONFIG="${POLICY_CONFIG:-pi05_510_erase_board_350}"
 
-# # 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
-# POLICY_DIR="${POLICY_DIR:-ckpt/lihong/510_erase_board_350_vision_50000}"
+# 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
+POLICY_DIR="${POLICY_DIR:-ckpt/lihong/510_erase_board_350_vision_50000}"
 
-# # 5. 任务 Prompt 提示词（输入给模型的语言指令）
-# PROMPT="${PROMPT:-Pick up the blackboard eraser and wipe off the blackboard}"
+# 5. 任务 Prompt 提示词（输入给模型的语言指令）
+PROMPT="${PROMPT:-Pick up the blackboard eraser and wipe off the blackboard}"
 
 
 # towel hanging
