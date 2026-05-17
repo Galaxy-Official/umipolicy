554045bc0e3a1453da7092cbe01fdc5e48811715
LihongGroup <lihonggroup@outlook.com>
2026-05-17 13:32:14 +0800

    Auto send: 2026-05-17 13:32:14

--- openpi/run_inference_pi05.sh
+++ openpi/run_inference_pi05.sh
@@ -57,24 +57,24 @@ export RECORDING_INDEX="${RECORDING_INDEX:-}"
 
 # screw
 # # 3. 任务策略名称（需与 handcap_config.py 中的 registered name 对应）
-# POLICY_CONFIG="${POLICY_CONFIG:-pi05_513_screw_490}"
+POLICY_CONFIG="${POLICY_CONFIG:-pi05_513_screw_490}"
 
-# # 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
-# # POLICY_DIR="${POLICY_DIR:-ckpt/lihong/513_screw_vision_90000}"
-# POLICY_DIR="${POLICY_DIR:-ckpt/lihong/513_screw_490_vision_50000}"
+# 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
+# POLICY_DIR="${POLICY_DIR:-ckpt/lihong/513_screw_vision_90000}"
+POLICY_DIR="${POLICY_DIR:-ckpt/lihong/513_screw_490_vision_50000}"
 
-# # # 5. 任务 Prompt 提示词（输入给模型的语言指令）
-# PROMPT="${PROMPT:-pick up the red socket wrench to loose the screw on the board.}"
+# # 5. 任务 Prompt 提示词（输入给模型的语言指令）
+PROMPT="${PROMPT:-pick up the red socket wrench to loose the screw on the board.}"
 
 #  erase board
 # 3. 任务策略名称（需与 handcap_config.py 中的 registered name 对应）
-POLICY_CONFIG="${POLICY_CONFIG:-pi05_510_erase_board_350}"
+# POLICY_CONFIG="${POLICY_CONFIG:-pi05_510_erase_board_350}"
 
-# 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
-POLICY_DIR="${POLICY_DIR:-ckpt/lihong/510_erase_board_350_vision_50000}"
+# # 4. 策略权重路径（保存模型的 ckpt 文件夹相对路径）
+# POLICY_DIR="${POLICY_DIR:-ckpt/lihong/510_erase_board_350_vision_50000}"
 
-# 5. 任务 Prompt 提示词（输入给模型的语言指令）
-PROMPT="${PROMPT:-Pick up the blackboard eraser and wipe off the blackboard}"
+# # 5. 任务 Prompt 提示词（输入给模型的语言指令）
+# PROMPT="${PROMPT:-Pick up the blackboard eraser and wipe off the blackboard}"
 
 
 # towel hanging
